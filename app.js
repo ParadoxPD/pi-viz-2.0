ea456a4565993552565d5529faf06a24adfc426b
ParadoxPD <ask.priyanshu@gmail.com>
2022-12-31 01:36:38 +0530

    Added app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+
+function setup() {
+    createCanvas(width, height);
+}
+
+
+function draw() {
+    background(0);
+    translate(width / 2, height / 2);
+}
